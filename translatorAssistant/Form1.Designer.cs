02bb55a34320fd1e841e965de29720ab1b8ebe13
adamkhasan09 <tigerko9999@gmail.com>
2021-09-13 14:57:00 +0300

    version 1.0

---
+++ translatorAssistant/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace translatorAssistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Key code demical:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(184, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(91, 35);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "39";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Браузер:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Сервис:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Multitran"});
+            this.comboBox2.Location = new System.Drawing.Point(184, 93);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(232, 37);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.Text = "Google translate";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLabel1.Location = new System.Drawing.Point(281, 16);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(135, 20);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Посмотреть код";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(268, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 44);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(184, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(232, 35);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "chrome.exe";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(199, 136);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(217, 24);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Удалить перенос строки";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(460, 222);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TranslatorAssistant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ translatorAssistant/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// 1. Import the InteropServices type
+using System.Threading;
+using System.Runtime.InteropServices;
+
+
+namespace translatorAssistant
+{
+    
+    public partial class Form1 : Form
+    {
+        string querySTR = "";
+        string urlMT = "https://www.multitran.com/m.exe?l1=1&l2=2&s="; //посковая строка мультитрана
+        string urlGl = "https://translate.google.com/?hl=ru#view=home&op=translate&sl=auto&tl=ru&text="; //посковая строка гугла
+        string sourse = "Google translate";
+        string outApp = "chrome.exe";
+        int HotKeyCode = 39;
+        int UniqueHotkeyId;
+        bool nDelFlag = false;
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        public const int KEYEVENTF_KEYDOWN = 0x0000; // New definition
+        public const int KEYEVENTF_EXTENDEDKEY = 0x0001; //Key down flag
+        public const int KEYEVENTF_KEYUP = 0x0002; //Key up flag
+        public const int VK_LCONTROL = 0xA2; //Left Control key code
+        public const int A = 0x41; //A key code
+        public const int C = 0x43; //C key code
+        public Form1()
+        {
+            InitializeComponent();
+            // 3. Register HotKey
+            //кол-во регестрируемых элементов
+            UniqueHotkeyId = 1;
+            // устанавливаем триггер на Right
+            HotKeyCode = (int)Keys.Right;
+            // Register the "Right" hotkey
+            Boolean F9Registered = RegisterHotKey(
+                this.Handle, UniqueHotkeyId, 0x0000, HotKeyCode
+            );
+            // убеждаемся что прошла успешная регисрация
+            if (F9Registered)
+            {/* MessageBox.Show("Выбранная кнопка зарегистрировалась");*/}
+            else
+            {/*MessageBox.Show("Выбранная кнопка не зарегистрировалась");*/}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static void Ctrl_C_Emulate()
+        {
+            keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYDOWN, 0);
+            keybd_event(C, 0, 0, 0);
+            keybd_event(C, 0, 2, 0);
+            keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYUP, 0);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            //Захват события на нажатие кнопки
+            if (m.Msg == 0x0312)
+            {
+                int id = m.WParam.ToInt32();
+
+                if (id == 1)
+                {
+                    // эмулируем нажатие ctrl + c
+                    Ctrl_C_Emulate();
+                    // времменно отснавливаем процесс чтобы данные успели скопироваться в кэш
+                    Thread.Sleep(500);
+                    // берем данные с буфера
+                    string query = Clipboard.GetText();
+                    //MessageBox.Show(nDelFlag.ToString());
+                    nDelFlag = checkBox1.Checked;
+                    if (sourse == "Google translate")
+                    {
+
+                        if (nDelFlag)
+                        {
+                            query = query.Replace("\n", " ");
+                        }
+                        else
+                        {
+                            query = query.Replace("\n", "%0A");
+                        }
+                        query = query.Replace(" ", "%20");
+                        querySTR = urlGl + query;
+                    }
+                    else if (sourse == "Multitran")
+                    {
+                        querySTR = urlMT + query.Replace(" ", "+");
+                    }
+                    System.Diagnostics.Process.Start(outApp, querySTR);
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+        private void VisitLink()
+        {
+            // Change the color of the link text by setting LinkVisited
+            // to true.
+            linkLabel1.LinkVisited = true;
+            //Call the Process.Start method to open the default browser
+            //with a URL:
+            System.Diagnostics.Process.Start("https://sun9-61.userapi.com/impg/3U9wSt5ey4InxOZ6XauOI0SzCDu7kry8x-o6rg/YduOlXZLd-4.jpg?size=1220x475&quality=96&sign=023583015d06cca2d7c1516901a3cdd9&type=album");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            VisitLink();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // получаем новый код
+                HotKeyCode = int.Parse(textBox1.Text);
+                Boolean UnRegistered = UnregisterHotKey(this.Handle, UniqueHotkeyId);
+                Boolean Registered = RegisterHotKey(
+                this.Handle, UniqueHotkeyId, 0x0000, HotKeyCode
+                );
+                
+                // получаем внешнее приложение для запуска
+                outApp = textBox2.Text;
+                //  Обработка сервиса
+                int serviceId = comboBox2.SelectedIndex;
+                if (serviceId == 0)
+                    sourse = "Google translate";
+                if (serviceId == 1)
+                    sourse = "Multitran";
+                
+            }
+            catch
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+        }
+    }
+}
